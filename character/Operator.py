08948bcb6844a45d60083ca613d0df6159b25478
xanaduw <xanaduw@synology.com>
2014-02-22 17:54:55 +0800

    r176 | xrloong | 2010-10-22 23:05:03 +0800 (五, 22 10月 2010) | 1 line
    增加忘記加入的檔案。

---
+++ character/Operator.py
@@ -0,0 +1,65 @@
+class Operator:
+	availableOperation=[
+		'龜', '水', '錯', '龍',
+		'夾',
+		'起', '廖', '載', '聖',
+		'霜', '想', '怡', '穎',
+		'回', '同', '函', '區', '左',
+		'好', '志', '算', '湘', '纂', '膷',
+		'林', '爻', '卅', '丰', '鑫', '卌', '圭', '燚',
+	]
+
+	directionInfoList={
+		'龜':'*',
+		'龍':'*',
+		'纂':'|',
+		'算':'|',
+		'志':'|',
+		'霜':'|',
+		'想':'|',
+		'爻':'|',
+		'卅':'|',
+		'蚕':'|',
+		'湘':'-',
+		'好':'-',
+		'怡':'-',
+		'穎':'-',
+		'林':'-',
+		'鑫':'-',
+		'鴻':'-',
+		'載':'*',
+		'廖':'*',
+		'起':'*',
+		'夾':'*',
+		'燚':'*',
+		'回':'@',
+		'同':'@',
+		'函':'@',
+		'區':'@',
+		'左':'@',
+		'水':'*',
+	}
+
+	def __init__(self, name='龜'):
+		self.name=name
+
+	def __str__(self):
+		return "%s"%self.name
+
+	def isAvailableOperation(self):
+		return (self.getName() in Operator.availableOperation)
+
+	def getName(self):
+		return self.name
+
+	def setName(self, name):
+		self.name=name
+
+	def getDirection(self):
+		direction=Operator.directionInfoList.get(self.name, '*')
+		return direction
+
+class TemplateOperator(Operator):
+	def __init(self, templateDescription):
+		pass
+
