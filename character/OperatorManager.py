60bd07f75b6615c2b1541ef932988c48dce8654a
xanaduw <xanaduw@synology.com>
2014-02-22 17:56:26 +0800

    r207 | xrloong | 2010-11-08 16:13:05 +0800 (一, 08 11月 2010) | 1 line
    重構。

--- character/OperatorManager.py
+++ character/OperatorManager.py
@@ -1,6 +1,40 @@
-from operatorinfo.RearrangeInfo import *
 from .CharDesc import CharDesc
 from character import Operator
+
+class RearrangeInfoSame:
+	def __init__(self):
+		pass
+
+	def rearrange(self, charDesc):
+		pass
+
+class RearrangeInfoSurroundingOpenUp:
+	def __init__(self, OperatorLoong):
+		self.operatorLoong=OperatorLoong
+
+	def rearrange(self, charDesc):
+		oldCompList=charDesc.getCompList()
+
+		ansOperator=self.operatorLoong
+		x=oldCompList[0]
+		y=oldCompList[1]
+		charDesc.setCompList([y, x])
+		charDesc.setOperator(ansOperator)
+
+class RearrangeInfoLShapeSimpleRadical:
+	def __init__(self, OperatorLoong):
+		self.operatorLoong=OperatorLoong
+
+	def rearrange(self, charDesc):
+		oldCompList=charDesc.getCompList()
+
+		ansOperator=self.operatorLoong
+		x=oldCompList[0]
+		y=oldCompList[1]
+
+		if x.getName() in ['辶', '廴']:
+			charDesc.setCompList([y, x])
+			charDesc.setOperator(ansOperator)
 
 class OperatorManager:
 	def __init__(self, descMgr, emptyCharDescGenerator):
