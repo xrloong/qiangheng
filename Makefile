34b6f1e9d026216e5e9404905eae40f875ebda85
xanaduw <xanaduw@synology.com>
2014-02-22 17:48:02 +0800

    r25 | xrloong | 2009-09-30 01:13:40 +0800 (三, 30  9月 2009) | 1 line
    修訂行列、大易、嘸蝦米組合字碼的規則，看起來更正確

--- Makefile
+++ Makefile
@@ -5,9 +5,9 @@ STYLELIST	=	scim gcin ime
 
 all:
 	for im in $(IMLIST);\
-		do ./qiangheng.py -g $$im -s scim >> scim/$$im.scim.txt;\
-		./qiangheng.py -g $$im -s gcin >> gcin/$$im.gcin.txt;\
-		./qiangheng.py -g $$im -s msim >> msim/$$im.msim.txt;\
+		do ./qiangheng.py -g $$im -s scim > scim/$$im.scim.txt;\
+		./qiangheng.py -g $$im -s gcin > gcin/$$im.gcin.txt;\
+		./qiangheng.py -g $$im -s msim > msim/$$im.msim.txt;\
 	done
 
 tarball: all
--- im.py
+++ im.py
@@ -6,7 +6,19 @@ class NoneIM:
 		pass
 
 	def genTable(self, chdict):
-		return []
+		table=[]
+		for chname, ch in chdict.items():
+			self.setCharTree(ch, chdict)
+			code=self.getCode(ch)
+			if code:
+				table.append([code, chname])
+		return table
+
+	def getCode(self, ch):
+		pass
+
+	def setCharTree(self, ch, chdict):
+		pass
 
 	def genCompList(self, ch, chdict):
 		if ch.structure[1] in ['龜']:
@@ -17,6 +29,36 @@ class NoneIM:
 			if x and y:
 				return self.genCompList(x,  chdict)+self.genCompList(y, chdict)
 		return []
+
+	def getAllComp(self, ch, chdict):
+#		['水', '林', '爻', '卅', '丰', '鑫', '卌', '圭', '燚',]
+#		['好', '志',
+#		'回', '同', '函', '區', '左',
+#		'起', '廖', '載', '聖', '句',
+#		'夾', '衍', '衷',]
+#		['纂', '膷',]
+		if ch.structure[1] in ['龜']:
+			return []
+		elif ch.structure[1] in ['好', '志', '回', '同', '函', '區', '載', '廖', '起']:
+			x=chdict.get(ch.structure[3], None)
+			y=chdict.get(ch.structure[4], None)
+			return [x, y]
+		elif ch.structure[1] in ['算', '湘', '霜', '想', '怡', '穎',]:
+			x=chdict.get(ch.structure[3], None)
+			y=chdict.get(ch.structure[4], None)
+			z=chdict.get(ch.structure[5], None)
+			return [x, y, z]
+		elif ch.structure[1] in ['林', '爻']:
+			x=chdict.get(ch.structure[3], None)
+			return [x, x]
+		elif ch.structure[1] in ['卅', '鑫']:
+			x=chdict.get(ch.structure[3], None)
+			return [x, x, x]
+		elif ch.structure[1] in ['燚',]:
+			x=chdict.get(ch.structure[3], None)
+			return [x, x, x, x]
+		else:
+			return []
 
 class CangJie(NoneIM):
 	"倉頡輸入法"
@@ -37,19 +79,27 @@ class Array(NoneIM):
 	def __init__(self):
 		pass
 
-	def genTable(self, chdict):
-		table=[]
-		for chname, ch in chdict.items():
-			complist=self.genCompList(ch, chdict)
-			arlist=list(map(lambda c: c.ar, complist))
-			if complist and all(arlist):
-				cat="".join(arlist)
-				if len(cat)>4:
-					ch.ar=(cat[:3]+cat[-1])
-				else:
-					ch.ar=(cat)
-				table.append([ch.ar, chname])
-		return table
+	def getCode(self, ch):
+		if ch.ar:
+			return ch.ar
+
+	def setCharTree(self, ch, chdict):
+		if ch.ar:
+			return
+
+		complist=self.getAllComp(ch, chdict)
+
+		for tmpch in complist:
+			self.setCharTree(tmpch, chdict)
+
+		arlist=list(map(lambda c: c.ar, complist))
+		if complist and all(arlist):
+			cat="".join(arlist)
+			if len(cat)>4:
+				ch.ar=(cat[:3]+cat[-1])
+			else:
+				ch.ar=(cat)
+
 
 class DaYi(NoneIM):
 	"大易輸入法"
@@ -57,19 +107,26 @@ class DaYi(NoneIM):
 	def __init__(self):
 		pass
 
-	def genTable(self, chdict):
-		table=[]
-		for chname, ch in chdict.items():
-			complist=self.genCompList(ch, chdict)
-			dylist=list(map(lambda c: c.dy, complist))
-			if complist and all(dylist):
-				cat="".join(dylist)
-				if len(cat)>4:
-					ch.dy=(cat[:3]+cat[-1])
-				else:
-					ch.dy=(cat)
-				table.append([ch.dy, chname])
-		return table
+	def getCode(self, ch):
+		if ch.dy:
+			return ch.dy
+
+	def setCharTree(self, ch, chdict):
+		if ch.dy:
+			return
+
+		complist=self.getAllComp(ch, chdict)
+
+		for tmpch in complist:
+			self.setCharTree(tmpch, chdict)
+
+		dylist=list(map(lambda c: c.dy, complist))
+		if complist and all(dylist):
+			cat="".join(dylist)
+			if len(cat)>4:
+				ch.dy=(cat[:3]+cat[-1])
+			else:
+				ch.dy=(cat)
 
 class Boshiamy(NoneIM):
 	"嘸蝦米輸入法"
@@ -77,20 +134,27 @@ class Boshiamy(NoneIM):
 	def __init__(self):
 		pass
 
-	def genTable(self, chdict):
-		table=[]
-		for chname, ch in chdict.items():
-			complist=self.genCompList(ch, chdict)
-			bslist=list(map(lambda c: c.bs, complist))
-			if complist and all(bslist):
-				cat="".join(bslist)
-				if len(cat)>4:
-					ch.bs=(cat[:3]+cat[-1])
-				else:
-					ch.bs=(cat)
-				ch.bssp=complist[-1].bssp
-				table.append([ch.bscode, chname])
-		return table
+	def getCode(self, ch):
+		if ch.bscode:
+			return ch.bscode
+
+	def setCharTree(self, ch, chdict):
+		if ch.bs:
+			return
+
+		complist=self.getAllComp(ch, chdict)
+
+		for tmpch in complist:
+			self.setCharTree(tmpch, chdict)
+
+		bslist=list(map(lambda c: c.bs, complist))
+		if complist and all(bslist):
+			cat="".join(bslist)
+			if len(cat)>4:
+				ch.bs=(cat[:3]+cat[-1])
+			else:
+				ch.bs=(cat)
+			ch.bssp=complist[-1].bssp
 
 class ZhengMa(NoneIM):
 	"鄭碼輸入法"
