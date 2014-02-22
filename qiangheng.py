a9440379bb638f730aad21db26bfceaf3741651f
xanaduw <xanaduw@synology.com>
2014-02-22 17:57:42 +0800

    r233 | xrloong | 2010-11-29 02:17:27 +0800 (一, 29 11月 2010) | 1 line
    更新字根描述檔。

--- qiangheng.py
+++ qiangheng.py
@@ -137,7 +137,8 @@ class QiangHeng:
 	def genIMMapping(self, targetCharList):
 		charDescQueryer=self.descMgr.getCharDescQueryer()
 		table=[]
-		for charName in targetCharList:
+		for charName in sorted(targetCharList):
+			print("<!-- %s -->"%charName)
 			charDesc=charDescQueryer(charName)
 			codeList=self.hanziNetwork.getCodeList(charDesc)
 			for code in codeList:
