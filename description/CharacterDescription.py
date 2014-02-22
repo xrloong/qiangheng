ade01de3d7cb31fa7aa7f6f9dbc12a9e4801f7f8
xanaduw <xanaduw@synology.com>
2014-02-22 18:01:13 +0800

    r301 | xrloong | 2011-01-26 09:37:47 +0800 (三, 26  1月 2011) | 1 line
    補送遺漏的檔案。

---
+++ description/CharacterDescription.py
@@ -0,0 +1,12 @@
+
+class CharacterDescription:
+	def __init__(self, name, structList=[]):
+		self.name=name
+		self.structList=structList
+
+	def getName(self):
+		return self.name
+
+	def getStructureList(self):
+		return self.structList
+
