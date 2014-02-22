30072877841e8d1a53faa0fb4280304b096da811
xanaduw <xanaduw@synology.com>
2014-02-22 17:47:54 +0800

    r22 | xrloong | 2009-09-29 21:36:39 +0800 (二, 29  9月 2009) | 1 line
    修改行列、大易、嘸蝦米組字方法，可以產生部分組合字

--- char.py
+++ char.py
@@ -3,6 +3,8 @@ class Char:
 	def __init__(self, char, prop):
 		self.char=char
 
+		self.structure=prop[0]
+
 		self._cangjie=None
 		self._array=None
 		self._dayi=None
@@ -14,11 +16,17 @@ class Char:
 		if len(prop)>=8:
 			self.setIMproperty(prop)
 
+	def __str__(self):
+		return self.char
+
+	def __repr__(self):
+		return str(self)
+
 	def setIMproperty(self, prop):
 		self.cj=prop[1]
 		self.ar=prop[2]
 		self.dy=prop[3]
-		self.bs=[prop[4], prop[5]]
+		self.bs, self.bssp=prop[4], prop[5]
 		self.zm=[prop[6], prop[7]]
 
 	@property
@@ -55,22 +63,39 @@ class Char:
 			self._dayi=code
 
 	@property
-	def bs(self):
-		if self._boshiamy==None or self._boshiamy_supplement==None:
+	def bscode(self):
+		if self.bs==None or self.bssp==None:
 			return None
-		if len(self._boshiamy)<3:
-			return self._boshiamy+self._boshiamy_supplement
+		if len(self.bs)<3:
+			return self.bs+self.bssp
 		else:
-			return self._boshiamy
+			return self.bs
+
+	@property
+	def bs(self):
+		if self._boshiamy==None:
+			return None
+		return self._boshiamy
 
 	@bs.setter
 	def bs(self, code):
-		if code[0]=='XXXX' or code[1]=='XXXX':
+		if code=='XXXX':
 			self._boshiamy=None
+		else:
+			self._boshiamy=code
+
+	@property
+	def bssp(self):
+		if self._boshiamy_supplement==None:
+			return None
+		return self._boshiamy_supplement
+
+	@bssp.setter
+	def bssp(self, sp):
+		if sp=='XXXX':
 			self._boshiamy_supplement=None
 		else:
-			self._boshiamy=code[0]
-			self._boshiamy_supplement=code[1]
+			self._boshiamy_supplement=sp
 
 	@property
 	def zm(self):
@@ -82,16 +107,24 @@ class Char:
 	def zm(self, code):
 		if code[0]=='XXXX' or code[1]=='XXXX':
 			self._zhengma=None
-			self._zhengma_type=None
+			self.zmtp=None
 		else:
 			self._zhengma=code[0]
-			self._zhengma_type=code[1]
+			self.zmtp=code[1]
+
+	@property
+	def zmtp(self):
+		return self._zhengma_type
+
+	@zmtp.setter
+	def zmtp(self, type):
+		self._zhengma_type=type
 
 if __name__=='__main__':
-	c=Char('王', ['(龜)', 'kn', 'sl', '/c', 'k', 'l', 'qy', '12'])
-	print('倉頡 ', c.cj)
-	print('行列 ', c.ar)
-	print('大易 ', c.dy)
-	print('嘸蝦米 ', c.bs)
-	print('鄭碼 ', c.zm)
+	c=Char('王', ['(龜)', 'hn', 'sl', '/c', 'k', 'l', 'qy', '12'])
+	print('倉頡', c.cj)
+	print('行列', c.ar)
+	print('大易', c.dy)
+	print('嘸蝦米', c.bs, c.bssp)
+	print('鄭碼', c.zm, c.zmtp)
 
--- im.py
+++ im.py
@@ -8,6 +8,16 @@ class NoneIM:
 	def genTable(self, chdict):
 		return []
 
+	def genCompList(self, ch, chdict):
+		if ch.structure[1] in ['龜']:
+			return [ch]
+		elif ch.structure[1] in ['好', '志']:
+			x=chdict.get(ch.structure[3], None)
+			y=chdict.get(ch.structure[4], None)
+			if x and y:
+				return self.genCompList(x,  chdict)+self.genCompList(y, chdict)
+		return []
+
 class CangJie(NoneIM):
 	"倉頡輸入法"
 
@@ -30,7 +40,14 @@ class Array(NoneIM):
 	def genTable(self, chdict):
 		table=[]
 		for chname, ch in chdict.items():
-			if ch.ar:
+			complist=self.genCompList(ch, chdict)
+			arlist=list(map(lambda c: c.ar, complist))
+			if complist and all(arlist):
+				cat="".join(arlist)
+				if len(cat)>4:
+					ch.ar=(cat[:3]+cat[-1])
+				else:
+					ch.ar=(cat)
 				table.append([ch.ar, chname])
 		return table
 
@@ -43,7 +60,14 @@ class DaYi(NoneIM):
 	def genTable(self, chdict):
 		table=[]
 		for chname, ch in chdict.items():
-			if ch.dy:
+			complist=self.genCompList(ch, chdict)
+			dylist=list(map(lambda c: c.dy, complist))
+			if complist and all(dylist):
+				cat="".join(dylist)
+				if len(cat)>4:
+					ch.dy=(cat[:3]+cat[-1])
+				else:
+					ch.dy=(cat)
 				table.append([ch.dy, chname])
 		return table
 
@@ -56,8 +80,16 @@ class Boshiamy(NoneIM):
 	def genTable(self, chdict):
 		table=[]
 		for chname, ch in chdict.items():
-			if ch.bs:
-				table.append([ch.bs, chname])
+			complist=self.genCompList(ch, chdict)
+			bslist=list(map(lambda c: c.bs, complist))
+			if complist and all(bslist):
+				cat="".join(bslist)
+				if len(cat)>4:
+					ch.bs=(cat[:3]+cat[-1])
+				else:
+					ch.bs=(cat)
+				ch.bssp=complist[-1].bssp
+				table.append([ch.bscode, chname])
 		return table
 
 class ZhengMa(NoneIM):
@@ -72,4 +104,7 @@ class ZhengMa(NoneIM):
 			if ch.zm:
 				table.append([ch.zm, chname])
 		return table
+
+if __name__=='__main__':
+	pass
 
