cc75ab7761844b7ae959dbc1d560f44d45259f6c
xanaduw <xanaduw@synology.com>
2014-02-22 18:01:52 +0800

    r315 | xrloong | 2011-02-11 12:20:53 +0800 (五, 11  2月 2011) | 1 line
    將剖析器的部分獨立出來。

--- src/description/CharDescriptionManager.py
+++ src/description/CharDescriptionManager.py
@@ -3,9 +3,10 @@
 from .CharacterDescription import CharacterDescription
 #from .StructureDescription import StructureDescription
 from .StructureDescription import HangerStructureDescription
-from .TemplateDesc import TemplateDesc
-from .TemplateDesc import TemplateCondition
+#from .TemplateDesc import TemplateDesc
+#from .TemplateDesc import TemplateCondition
 from .operator import OperatorManager
+from parser import QHParser
 from xml.etree import ElementTree
 
 class CharDescriptionManager:
@@ -32,6 +33,8 @@ class CharDescriptionManager:
 			codeInfoDictList=self.propertyDB.get(charName, [])
 			return codeInfoDictList
 
+		self.parser=QHParser.QHParser(structDescGenerator)
+
 		imName=imModule.IMInfo.IMName
 		self.operationMgr=OperatorManager.OperatorManager(self)
 
@@ -45,12 +48,6 @@ class CharDescriptionManager:
 	def keys(self):
 		return self.characterDB.keys()
 
-	def getStructDescGenerator(self):
-		return self.structDescGenerator
-
-#	def getStructDescQueryer(self):
-#		return self.structDescQueryer
-
 	def getCharDescQueryer(self):
 		return self.charDescQueryer
 
@@ -61,195 +58,30 @@ class CharDescriptionManager:
 		f=open(filename, encoding=fileencoding)
 		xmlNode=ElementTree.parse(f)
 		rootNode=xmlNode.getroot()
-		version=rootNode.get('版本號')
-		if version=='0.2':
-			self.loadCharDescriptionByParsingXML__0_2(rootNode)
-
-	def loadTemplateFromXML(self, filename, fileencoding='utf-8-sig'):
-		f=open(filename, encoding=fileencoding)
-		xmlNode=ElementTree.parse(f)
-		rootNode=xmlNode.getroot()
-		version=rootNode.get('版本號')
-		if version=='0.2':
-			self.loadTemplateByParsingXML__0_2(rootNode)
-
-	def loadCodeInfoFromXML(self, filename, fileencoding='utf-8-sig'):
-		f=open(filename, encoding=fileencoding)
-		xmlNode=ElementTree.parse(f)
-		rootNode=xmlNode.getroot()
-		version=rootNode.get('版本號')
-		if version=='0.2':
-			self.loadCodeInfoByParsingXML__0_2(rootNode)
-
-
-	def loadByParsingXML__0_2(self, rootNode):
-		self.loadTemplateByParsingXML__0_2(rootNode)
-		self.loadCharDescriptionByParsingXML__0_2(rootNode)
-		self.loadCodeInfoByParsingXML__0_2(rootNode)
-
-	def loadCharDescriptionByParsingXML__0_2(self, rootNode):
-		# 用於 0.2 版
-		structDescGenerator=self.getStructDescGenerator()
-
-		def getDesc_AssembleChar(assembleChar):
-			l=[]
-			operatorName=assembleChar.get("運算")
-			filter_lambda=lambda x: x.tag in ["字根", "組字", "套用範本"]
-			targetChildNodes=filter(filter_lambda , list(assembleChar))
-			for node in targetChildNodes:
-				if node.tag=="字根":
-					name=node.get("置換")
-					structDesc=structDescGenerator()
-					structDesc.setExpandName(name)
-					l.append(structDesc)
-				elif node.tag=="組字":
-					l.append(getDesc_AssembleChar(node))
-				else:
-					pass
-
-			propDict=assembleChar.attrib
-			if operatorName:
-				comp=structDescGenerator([operatorName, l])
-			else:
-				comp=structDescGenerator()
-
-			return comp
-
-		def getDesc_SubCharacter(nodeCharacter):
-			assembleCharList=nodeCharacter.findall("組字")
-			compList=[]
-			for assembleChar in assembleCharList:
-				comp=getDesc_AssembleChar(assembleChar)
-
-				comp.setStructureProperties(assembleChar.attrib)
-
-				compList.append(comp)
-			return compList
-
-		def getDesc_CompleteCharacterList(nodeCharacter):
-			compList=getDesc_SubCharacter(nodeCharacter)
-			return compList
-
-		charGroupNode=rootNode.find("字符集")
-		targetChildNodes=charGroupNode.findall("字符")
-		for node in targetChildNodes:
-			compList=getDesc_CompleteCharacterList(node)
-			charName=node.get('名稱')
-			for comp in compList:
-				comp.setExpandName(charName)
-
+		nodeInfoList=self.parser.loadCharDescriptionByParsingXML(rootNode)
+		for [charName, compList, propDict] in nodeInfoList:
 			charDesc=self.characterDB.get(charName, None)
 			if charDesc==None:
 				charDesc=CharacterDescription(charName)
 				self.characterDB[charName]=charDesc
 			charDesc.setStructureList(compList)
-			charDesc.updateProperty(node.attrib)
-				
-#			self.characterDB[charName]=CharacterDescription(charName, compList)
+			charDesc.updateProperty(propDict)
 
-	def loadTemplateByParsingXML__0_2(self, rootNode):
-		# 用於 0.2 版
-		structDescGenerator=self.getStructDescGenerator()
 
-		def getDesc_AssembleChar(assembleChar):
-			l=[]
-			operatorName=assembleChar.get("運算")
-			filter_lambda=lambda x: x.tag in ["字根", "組字", "套用範本"]
-			targetChildNodes=filter(filter_lambda , list(assembleChar))
-			for node in targetChildNodes:
-				if node.tag=="字根":
-					name=node.get("置換")
-					structDesc=structDescGenerator()
-					structDesc.setExpandName(name)
-					l.append(structDesc)
-				elif node.tag=="組字":
-					l.append(getDesc_AssembleChar(node))
-				else:
-					pass
+	def loadTemplateFromXML(self, filename, fileencoding='utf-8-sig'):
+		f=open(filename, encoding=fileencoding)
+		xmlNode=ElementTree.parse(f)
+		rootNode=xmlNode.getroot()
+		templateDB=self.parser.loadTemplateByParsingXML(rootNode)
+		self.operationMgr.setTemplateDB(templateDB)
 
-			if operatorName:
-				comp=structDescGenerator([operatorName, l])
-			else:
-				comp=structDescGenerator()
+	def loadCodeInfoFromXML(self, filename, fileencoding='utf-8-sig'):
+		f=open(filename, encoding=fileencoding)
+		xmlNode=ElementTree.parse(f)
+		rootNode=xmlNode.getroot()
+		propertyDB=self.parser.loadCodeInfoByParsingXML(rootNode)
+		self.propertyDB.update(propertyDB)
 
-			return comp
-
-		def getDesc_ArgumentList(nodeArgument):
-			argumentList=[]
-			targetArgumentNodes=nodeArgument.findall("引數")
-			for node in targetArgumentNodes:
-				charName=node.get('名稱')
-				argumentList.append(charName)
-			return argumentList
-
-		def getDesc_ParameterList(nodeParameter):
-			parameterList=[]
-			targetParameterNodes=nodeParameter.findall("參數")
-			for node in targetParameterNodes:
-				charName=node.get('名稱')
-				parameterList.append(charName)
-			return parameterList
-
-		def getDesc_Template_Structure(nodeStructure):
-			condition=None
-
-			conditionNode=nodeStructure.find("條件式")
-			if conditionNode!=None:
-				operator=conditionNode.get('運算')
-				operand1=conditionNode.get('運算元一')
-				operand2=conditionNode.get('運算元二')
-				condition=TemplateCondition([operator, operand1, operand2])
-			else:
-				condition=TemplateCondition()
-
-			assembleChar=nodeStructure.find("組字")
-			comp=getDesc_AssembleChar(assembleChar)
-			return [condition, comp]
-
-		def getDesc_Template(nodeTemplate):
-			templateName=nodeTemplate.get('名稱')
-
-			parameterNodeList=nodeTemplate.find("參數列")
-			parameterNameList=getDesc_ParameterList(parameterNodeList)
-
-			replaceInfoList=[]
-			structureNodes=nodeTemplate.findall("組字結構")
-			for node in structureNodes:
-				replaceInfo=getDesc_Template_Structure(node)
-				replaceInfoList.append(replaceInfo)
-			[condition, comp]=replaceInfoList[0]
-
-			return TemplateDesc(templateName, replaceInfoList, parameterNameList)
-
-		templateGroupNode=rootNode.find("範本集")
-		if None!=templateGroupNode:
-			targetChildNodes=templateGroupNode.findall("範本")
-			for node in targetChildNodes:
-				templateName=node.get('名稱')
-				templateDesc=getDesc_Template(node)
-				self.templateDB[templateName]=templateDesc
-			self.operationMgr.setTemplateDB(self.templateDB)
-
-	def loadCodeInfoByParsingXML__0_2(self, rootNode):
-		# 用於 0.2 版
-		def getDesc_CodeInfoList(nodeCharacter):
-			assembleCharList=nodeCharacter.findall("組字")
-			infoDictList=[]
-			for assembleChar in assembleCharList:
-				infoDict=None
-				codeInfo=assembleChar.find("編碼資訊")
-				if codeInfo is not None:
-					infoDict=codeInfo.attrib
-
-				infoDictList.append(infoDict)
-			return infoDictList
-
-		charGroupNode=rootNode.find("字符集")
-		targetChildNodes=charGroupNode.findall("字符")
-		for node in targetChildNodes:
-			charName=node.get('名稱')
-			codeInfoDictList=getDesc_CodeInfoList(node)
-			self.propertyDB[charName]=codeInfoDictList
 
 	def adjustData(self):
 		self.operationMgr.adjustTemplate()
---
+++ src/parser/QHParser.py
@@ -0,0 +1,156 @@
+from description.TemplateDesc import TemplateDesc
+from description.TemplateDesc import TemplateCondition
+
+class QHParser:
+	def __init__(self, structDescGenerator):
+		self.structDescGenerator=structDescGenerator
+
+	def getDesc_AssembleChar(self, assembleChar):
+		l=[]
+		operatorName=assembleChar.get("運算")
+		filter_lambda=lambda x: x.tag in ["字根", "組字", "套用範本"]
+		targetChildNodes=filter(filter_lambda , list(assembleChar))
+		for node in targetChildNodes:
+			if node.tag=="字根":
+				name=node.get("置換")
+				structDesc=self.structDescGenerator()
+				structDesc.setExpandName(name)
+				l.append(structDesc)
+			elif node.tag=="組字":
+				l.append(self.getDesc_AssembleChar(node))
+			else:
+				pass
+
+		propDict=assembleChar.attrib
+		if operatorName:
+			comp=self.structDescGenerator([operatorName, l])
+		else:
+			comp=self.structDescGenerator()
+
+		return comp
+
+	def getDesc_SubCharacter(self, nodeCharacter):
+		assembleCharList=nodeCharacter.findall("組字")
+		compList=[]
+		for assembleChar in assembleCharList:
+			comp=self.getDesc_AssembleChar(assembleChar)
+
+			comp.setStructureProperties(assembleChar.attrib)
+
+			compList.append(comp)
+		return compList
+
+	def getDesc_CompleteCharacterList(self, nodeCharacter):
+		compList=self.getDesc_SubCharacter(nodeCharacter)
+		return compList
+
+	def getDesc_ParameterList(self, nodeParameter):
+		parameterList=[]
+		targetParameterNodes=nodeParameter.findall("參數")
+		for node in targetParameterNodes:
+			charName=node.get('名稱')
+			parameterList.append(charName)
+		return parameterList
+
+	def getDesc_Template_Structure(self, nodeStructure):
+		condition=None
+
+		conditionNode=nodeStructure.find("條件式")
+		if conditionNode!=None:
+			operator=conditionNode.get('運算')
+			operand1=conditionNode.get('運算元一')
+			operand2=conditionNode.get('運算元二')
+			condition=TemplateCondition([operator, operand1, operand2])
+		else:
+			condition=TemplateCondition()
+
+		assembleChar=nodeStructure.find("組字")
+		comp=self.getDesc_AssembleChar(assembleChar)
+		return [condition, comp]
+
+	def getDesc_Template(self, nodeTemplate):
+		templateName=nodeTemplate.get('名稱')
+
+		parameterNodeList=nodeTemplate.find("參數列")
+		parameterNameList=self.getDesc_ParameterList(parameterNodeList)
+
+		replaceInfoList=[]
+		structureNodes=nodeTemplate.findall("組字結構")
+		for node in structureNodes:
+			replaceInfo=self.getDesc_Template_Structure(node)
+			replaceInfoList.append(replaceInfo)
+		[condition, comp]=replaceInfoList[0]
+
+		return TemplateDesc(templateName, replaceInfoList, parameterNameList)
+
+	def getDesc_CodeInfoList(self, nodeCharacter):
+		assembleCharList=nodeCharacter.findall("組字")
+		infoDictList=[]
+		for assembleChar in assembleCharList:
+			infoDict=None
+			codeInfo=assembleChar.find("編碼資訊")
+			if codeInfo is not None:
+				infoDict=codeInfo.attrib
+
+			infoDictList.append(infoDict)
+		return infoDictList
+
+	def loadCodeInfoByParsingXML__0_2(self, rootNode):
+		# 用於 0.2 版
+		charGroupNode=rootNode.find("字符集")
+		targetChildNodes=charGroupNode.findall("字符")
+		propertyDB={}
+		for node in targetChildNodes:
+			charName=node.get('名稱')
+			codeInfoDictList=self.getDesc_CodeInfoList(node)
+			propertyDB[charName]=codeInfoDictList
+		return propertyDB
+
+	def loadTemplateByParsingXML__0_2(self, rootNode):
+		# 用於 0.2 版
+		templateGroupNode=rootNode.find("範本集")
+		templateDB={}
+		if None!=templateGroupNode:
+			targetChildNodes=templateGroupNode.findall("範本")
+			for node in targetChildNodes:
+				templateName=node.get('名稱')
+				templateDesc=self.getDesc_Template(node)
+				templateDB[templateName]=templateDesc
+		return templateDB
+
+	def loadCharDescriptionByParsingXML__0_2(self, rootNode):
+		# 用於 0.2 版
+		charGroupNode=rootNode.find("字符集")
+		targetChildNodes=charGroupNode.findall("字符")
+
+		nodeInfoList=[]
+		for node in targetChildNodes:
+			compList=self.getDesc_CompleteCharacterList(node)
+			charName=node.get('名稱')
+			for comp in compList:
+				comp.setExpandName(charName)
+
+			nodeInfoList.append([charName, compList, node.attrib])
+		return nodeInfoList
+
+	def loadCodeInfoByParsingXML(self, node):
+		version=node.get('版本號')
+		propertyDB={}
+		if version=='0.2':
+			propertyDB=self.loadCodeInfoByParsingXML__0_2(node)
+		return propertyDB
+
+	def loadTemplateByParsingXML(self, node):
+		version=node.get('版本號')
+		templateDB={}
+		if version=='0.2':
+			templateDB=self.loadTemplateByParsingXML__0_2(node)
+		return templateDB
+
+	def loadCharDescriptionByParsingXML(self, node):
+		version=node.get('版本號')
+		nodeInfoList=[]
+		if version=='0.2':
+			nodeInfoList=self.loadCharDescriptionByParsingXML__0_2(node)
+		return nodeInfoList
+
