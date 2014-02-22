8dc45de60ba09531ae03b7783ce0b2af45787c29
xanaduw <xanaduw@synology.com>
2014-02-22 18:04:00 +0800

    r357 | xrloong | 2011-04-18 10:01:22 +0800 (一, 18  4月 2011) | 1 line
    重構，將 HanZiNode 由 HanZiNetwork 中移出。

--- src/hanzi/HanZiNetwork.py
+++ src/hanzi/HanZiNetwork.py
@@ -1,179 +1,8 @@
 import copy
-from state import StateManager
 from description.CodeType import CodeType
 from description.operator import Operator
 
-class HanZiCodeInfo:
-	def __init__(self, propDict, codeType,):
-		codeInfo=StateManager.codeInfoGenerator(propDict)
-		self.codeInfo=codeInfo
-		self.codeInfo.multiCodeType(codeType)
-
-		hasCharacter=bool("字符碼" in propDict)
-		hasRadix=bool("字根碼" in propDict)
-		if hasCharacter or hasRadix:
-			self._isSupportCharacterCode=False
-			self._isSupportRadixCode=False
-			if hasCharacter:
-				self._isSupportCharacterCode=True
-			if hasRadix:
-				self._isSupportRadixCode=True
-		else:
-			self._isSupportCharacterCode=True
-			self._isSupportRadixCode=True
-
-	def isSupportCharacterCode(self):
-		return self._isSupportCharacterCode
-
-	def isSupportRadixCode(self):
-		return self._isSupportRadixCode
-
-	def setRadixCodeProperties(self, propDict):
-		self.codeInfo.setRadixCodeProperties(propDict)
-		pass
-
-	def setCompositions(self, operator, complist):
-		tmpCompList=[comp.codeInfo for comp in complist]
-		self.codeInfo.setCompositions(operator, tmpCompList)
-
-	def getCodeType(self):
-		return self.codeInfo.getCodeType()
-
-	def getCodeProperties(self):
-		return self.codeInfo.getCodeProperties()
-
-class HanZiStructure:
-	def __init__(self, codeType, operator, nodeList):
-		self.operator=operator
-		self.nodeList=nodeList
-
-		self.codeInfoList=[]
-		self.codeType=codeType
-
-		self.flagIsSet=True
-
-	def setToRadix(self):
-		self.flagIsSet=False
-
-	def setToComponent(self):
-		pass
-
-	def getOperator(self):
-		return self.operator
-
-	def getNodeList(self):
-		return self.nodeList
-
-	def appendCodeInfo(self, codeInfo):
-		self.codeInfoList.append(codeInfo)
-
-	def getCodeInfoList(self):
-		return self.codeInfoList
-
-	def setStructure(self, operator, nodeList):
-		self.operator=operator
-		self.nodeList=nodeList
-
-	def setCompositions(self):
-		if self.flagIsSet:
-			return
-		self.flagIsSet=True
-
-		nodeList=self.nodeList
-		infoListList=self.getAllCodeInfoList(nodeList)
-
-		for infoList in infoListList:
-			codeInfo=HanZiCodeInfo({}, self.codeType)
-			codeInfo.setCompositions(self.getOperator(), infoList)
-
-			self.appendCodeInfo(codeInfo)
-
-	def getAllCodeInfoList(self, nodeList):
-		def combineList(infoListList, infoListOfNode):
-			if len(infoListList)==0:
-				ansListList=[]
-				for codeInfo in infoListOfNode:
-					ansListList.append([codeInfo])
-			else:
-				ansListList=[]
-				for infoList in infoListList:
-					for codeInfo in infoListOfNode:
-						ansListList.append(infoList+[codeInfo])
-
-			return ansListList
-
-		infoListList=[]
-
-		for node in nodeList:
-			tmpCodeInfoList=node.getCodeInfoList()
-			codeInfoList=filter(lambda x: x.isSupportRadixCode(), tmpCodeInfoList)
-			infoListList=combineList(infoListList, codeInfoList)
-
-		return infoListList
-
-class HanZiNode:
-	def __init__(self, name):
-		self.name=name
-		self.structureList=[]
-
-	def addStructure(self, structure):
-		self.structureList.append(structure)
-
-	def setStructureList(self, structureList):
-		self.structureList=structureList
-
-	def getStructureListWithCondition(self):
-		return self.structureList
-
-	def getCodeInfoList(self):
-		structureList=self.getStructureListWithCondition()
-		return sum(map(lambda s: s.getCodeInfoList(), structureList), [])
-
-	def getCodePropertiesList(self):
-		self.setNodeTree()
-
-		codeList=[]
-		tmpCodeInfoList=self.getCodeInfoList()
-		codeInfoList=filter(lambda x: x.isSupportCharacterCode(), tmpCodeInfoList)
-		for codeInfo in codeInfoList:
-			codeProp=codeInfo.getCodeProperties()
-			if codeProp:
-				codeList.append(codeProp)
-		return codeList
-
-	def setNodeTree(self):
-		"""設定某一個字符所包含的部件的碼"""
-
-		structureList=self.getStructureListWithCondition()
-
-		for structure in structureList:
-			radixList=structure.getNodeList()
-			for childNode in radixList:
-				childNode.setNodeTree()
-
-			structure.setCompositions()
-
-class HanZiWrapperNode:
-	def __init__(self, targetNode):
-		self.targetNode=targetNode
-
-	def addStructure(self, structure):
-		self.targetNode.addStructure(structure)
-
-	def setStructureList(self, structureList):
-		self.targetNode.setStructureList(structureList)
-
-	def getStructureListWithCondition(self):
-		return self.targetNode.getStructureListWithCondition()
-
-	def getCodeInfoList(self):
-		return self.targetNode.getCodeInfoList()
-
-	def getCodePropertiesList(self):
-		return self.targetNode.getCodePropertiesList()
-
-	def setNodeTree(self):
-		return self.targetNode.setNodeTree()
+from . import HanZiNode
 
 class DescriptionManagerToHanZiNetworkConverter:
 	def __init__(self, descriptionManager):
@@ -231,21 +60,21 @@ class HanZiNetwork:
 		return toHanZiNetworkConverter.constructDescriptionNetwork()
 
 	def addNamedNode(self, name):
-		tmpNode=HanZiNode(name)
+		tmpNode=HanZiNode.HanZiNode(name)
 		self.structDescUniqueNameToNodeDict[name]=tmpNode
 		self.structDescExpandNameToNodeDict[name]=tmpNode
 
 	def addNode(self, structDesc):
 		anonymousName=structDesc.getUniqueName()
 		if anonymousName not in self.structDescUniqueNameToNodeDict:
-			tmpNode=HanZiNode(anonymousName)
+			tmpNode=HanZiNode.HanZiNode(anonymousName)
 			self.structDescUniqueNameToNodeDict[anonymousName]=tmpNode
 
 	def addReferenceNode(self, structDesc):
 		anonymousName=structDesc.getUniqueName()
 		if anonymousName not in self.structDescUniqueNameToNodeDict:
 			rootNode=self.structDescExpandNameToNodeDict.get(structDesc.getReferenceName())
-			tmpNode=HanZiWrapperNode(rootNode)
+			tmpNode=HanZiNode.HanZiWrapperNode(rootNode)
 			self.structDescUniqueNameToNodeDict[anonymousName]=tmpNode
 
 	def addTurtleStruct(self, structDesc):
@@ -258,9 +87,9 @@ class HanZiNetwork:
 		codeInfoProperties=structDesc.getCodeInfoDict()
 
 		codeType=structDesc.getCodeType()
-		structure=HanZiStructure(codeType, None, [])
+		structure=HanZiNode.HanZiStructure(codeType, None, [])
 
-		codeInfo=HanZiCodeInfo(codeInfoProperties, codeType)
+		codeInfo=HanZiNode.HanZiCodeInfo(codeInfoProperties, codeType)
 		structure.appendCodeInfo(codeInfo)
 
 		structure.setToComponent()
@@ -273,7 +102,7 @@ class HanZiNetwork:
 			dstNode=self.findNode(structDesc)
 
 			codeType=structDesc.getCodeType()
-			structure=HanZiStructure(codeType, operator, childNodeList)
+			structure=HanZiNode.HanZiStructure(codeType, operator, childNodeList)
 			structure.setToRadix()
 
 			dstNode.addStructure(structure)
---
+++ src/hanzi/HanZiNode.py
@@ -0,0 +1,175 @@
+from state import StateManager
+
+class HanZiCodeInfo:
+	def __init__(self, propDict, codeType,):
+		codeInfo=StateManager.codeInfoGenerator(propDict)
+		self.codeInfo=codeInfo
+		self.codeInfo.multiCodeType(codeType)
+
+		hasCharacter=bool("字符碼" in propDict)
+		hasRadix=bool("字根碼" in propDict)
+		if hasCharacter or hasRadix:
+			self._isSupportCharacterCode=False
+			self._isSupportRadixCode=False
+			if hasCharacter:
+				self._isSupportCharacterCode=True
+			if hasRadix:
+				self._isSupportRadixCode=True
+		else:
+			self._isSupportCharacterCode=True
+			self._isSupportRadixCode=True
+
+	def isSupportCharacterCode(self):
+		return self._isSupportCharacterCode
+
+	def isSupportRadixCode(self):
+		return self._isSupportRadixCode
+
+	def setRadixCodeProperties(self, propDict):
+		self.codeInfo.setRadixCodeProperties(propDict)
+		pass
+
+	def setCompositions(self, operator, complist):
+		tmpCompList=[comp.codeInfo for comp in complist]
+		self.codeInfo.setCompositions(operator, tmpCompList)
+
+	def getCodeType(self):
+		return self.codeInfo.getCodeType()
+
+	def getCodeProperties(self):
+		return self.codeInfo.getCodeProperties()
+
+class HanZiStructure:
+	def __init__(self, codeType, operator, nodeList):
+		self.operator=operator
+		self.nodeList=nodeList
+
+		self.codeInfoList=[]
+		self.codeType=codeType
+
+		self.flagIsSet=True
+
+	def setToRadix(self):
+		self.flagIsSet=False
+
+	def setToComponent(self):
+		pass
+
+	def getOperator(self):
+		return self.operator
+
+	def getNodeList(self):
+		return self.nodeList
+
+	def appendCodeInfo(self, codeInfo):
+		self.codeInfoList.append(codeInfo)
+
+	def getCodeInfoList(self):
+		return self.codeInfoList
+
+	def setStructure(self, operator, nodeList):
+		self.operator=operator
+		self.nodeList=nodeList
+
+	def setCompositions(self):
+		if self.flagIsSet:
+			return
+		self.flagIsSet=True
+
+		nodeList=self.nodeList
+		infoListList=self.getAllCodeInfoList(nodeList)
+
+		for infoList in infoListList:
+			codeInfo=HanZiCodeInfo({}, self.codeType)
+			codeInfo.setCompositions(self.getOperator(), infoList)
+
+			self.appendCodeInfo(codeInfo)
+
+	def getAllCodeInfoList(self, nodeList):
+		def combineList(infoListList, infoListOfNode):
+			if len(infoListList)==0:
+				ansListList=[]
+				for codeInfo in infoListOfNode:
+					ansListList.append([codeInfo])
+			else:
+				ansListList=[]
+				for infoList in infoListList:
+					for codeInfo in infoListOfNode:
+						ansListList.append(infoList+[codeInfo])
+
+			return ansListList
+
+		infoListList=[]
+
+		for node in nodeList:
+			tmpCodeInfoList=node.getCodeInfoList()
+			codeInfoList=filter(lambda x: x.isSupportRadixCode(), tmpCodeInfoList)
+			infoListList=combineList(infoListList, codeInfoList)
+
+		return infoListList
+
+class HanZiNode:
+	def __init__(self, name):
+		self.name=name
+		self.structureList=[]
+
+	def addStructure(self, structure):
+		self.structureList.append(structure)
+
+	def setStructureList(self, structureList):
+		self.structureList=structureList
+
+	def getStructureListWithCondition(self):
+		return self.structureList
+
+	def getCodeInfoList(self):
+		structureList=self.getStructureListWithCondition()
+		return sum(map(lambda s: s.getCodeInfoList(), structureList), [])
+
+	def getCodePropertiesList(self):
+		self.setNodeTree()
+
+		codeList=[]
+		tmpCodeInfoList=self.getCodeInfoList()
+		codeInfoList=filter(lambda x: x.isSupportCharacterCode(), tmpCodeInfoList)
+		for codeInfo in codeInfoList:
+			codeProp=codeInfo.getCodeProperties()
+			if codeProp:
+				codeList.append(codeProp)
+		return codeList
+
+	def setNodeTree(self):
+		"""設定某一個字符所包含的部件的碼"""
+
+		structureList=self.getStructureListWithCondition()
+
+		for structure in structureList:
+			radixList=structure.getNodeList()
+			for childNode in radixList:
+				childNode.setNodeTree()
+
+			structure.setCompositions()
+
+class HanZiWrapperNode:
+	def __init__(self, targetNode):
+		self.targetNode=targetNode
+
+	def addStructure(self, structure):
+		self.targetNode.addStructure(structure)
+
+	def setStructureList(self, structureList):
+		self.targetNode.setStructureList(structureList)
+
+	def getStructureListWithCondition(self):
+		return self.targetNode.getStructureListWithCondition()
+
+	def getCodeInfoList(self):
+		return self.targetNode.getCodeInfoList()
+
+	def getCodePropertiesList(self):
+		return self.targetNode.getCodePropertiesList()
+
+	def setNodeTree(self):
+		return self.targetNode.setNodeTree()
+
+
