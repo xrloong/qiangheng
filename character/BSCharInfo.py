8d6a756b9b64a629ced9ad3311e6934ac29b8f01
xanaduw <xanaduw@synology.com>
2014-02-22 17:59:50 +0800

    r273 | xrloong | 2011-01-12 22:30:50 +0800 (三, 12  1月 2011) | 1 line
    修改嘸蝦米編碼的表示方式。

--- character/BSCharInfo.py
+++ character/BSCharInfo.py
@@ -13,19 +13,14 @@ class BSCharInfo(CharInfo):
 
 	def setPropDict(self, propDict):
 		self._bs_single=propDict.get('獨體編碼')
-		str_rtlist=propDict.get('資訊表示式')
-		if str_rtlist!=None:
-			self.setBSProp(str_rtlist)
+		str_incode=propDict.get('資訊表示式')
+		str_spcode=propDict.get('嘸蝦米補碼')
+		if str_incode!=None and str_spcode!=None:
+			self.setBSProp(str_incode, str_spcode)
 
-	def setBSProp(self, bs_x_code):
-		bs_list=bs_x_code.split(',')
-
-		if len(bs_list)>=2 and bs_list[0] != 'XXXX' and bs_list[1]!='XXXX':
-			self._bs_incode=bs_list[0]
-			self._bs_spcode=bs_list[1]
-		else:
-			self._bs_incode=None
-			self._bs_spcode=None
+	def setBSProp(self, bs_incode, bs_spcode):
+		self._bs_incode=bs_incode
+		self._bs_spcode=bs_spcode
 
 		self.setFlag=True
 
@@ -38,7 +33,7 @@ class BSCharInfo(CharInfo):
 			cat="".join(bslist)
 			bs_incode=(cat[:3]+cat[-1]) if len(cat)>4 else cat
 			bs_spcode=complist[-1].getBSProp()[1]
-			self.setBSProp(','.join([bs_incode, bs_spcode]))
+			self.setBSProp(bs_incode, bs_spcode)
 
 	@property
 	def bs(self):
