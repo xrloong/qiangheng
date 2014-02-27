7e560fee1b90a0f00cba0207772c03e80e02a6a4
xrloong <xrloong@gmail.com>
2014-02-27 23:26:00 +0800

    移除行列的子字根機制，簡化資料結構及程式架構。

--- src/im/Array/ARRadixManager.py
+++ src/im/Array/ARRadixManager.py
@@ -8,20 +8,10 @@ class ARRadixParser(RadixParser):
 	RADIX_SEPERATOR=','
 
 	ATTRIB_CODE_EXPRESSION='資訊表示式'
-	TAG_SUB_RADIX='子字根'
-	ATTRIB_USE_RADIX='使用'
-
-	# 多型
-	def convertElementToRadixInfo(self, elementCodeInfo):
-		radixInfoDescription=ARRadixCodeInfoDescription(elementCodeInfo)
-		return radixInfoDescription
 
 	# 多型
 	def convertRadixDescToCodeInfo(self, radixDesc):
-		if(radixDesc.isWithExpression()):
-			codeInfo=self.convertRadixDescToCodeInfoByExpression(radixDesc)
-		else:
-			codeInfo=self.convertRadixDescToCodeInfoByReference(radixDesc)
+		codeInfo=self.convertRadixDescToCodeInfoByExpression(radixDesc)
 		return codeInfo
 
 	def convertRadixDescToCodeInfoByExpression(self, radixInfo):
@@ -40,38 +30,4 @@ class ARRadixParser(RadixParser):
 
 		codeInfo=ARCodeInfo(codeList)
 		return codeInfo
-
-	def convertRadixDescToCodeInfoByReference(self, radixDesc):
-		nameList=radixDesc.getRadixNameList()
-
-		codeList=[]
-		for radixName in nameList:
-			radixDesc=self.getRadixDescription(radixName)
-			radixCodeInfoList=self.convertRadixDescToCodeInfoList(radixDesc)
-
-			radixInfo=radixCodeInfoList[0]
-			radixCodeList=radixInfo.getMainCodeList()
-			codeList.append(radixCodeList)
-
-		codeInfo=ARCodeInfo(codeList)
-		return codeInfo
-
-class ARRadixCodeInfoDescription(RadixCodeInfoDescription):
-	def __init__(self, elementCodeInfo):
-		super().__init__(elementCodeInfo)
-
-		radixNameList=[]
-		subRadixNodeList=elementCodeInfo.findall(ARRadixParser.TAG_SUB_RADIX)
-		for subRadixNode in subRadixNodeList:
-			radixName=subRadixNode.attrib.get(ARRadixParser.ATTRIB_USE_RADIX)
-			radixNameList.append(radixName)
-
-		self.radixNameList=radixNameList
-
-	def getRadixNameList(self):
-		return self.radixNameList
-
-	def isWithExpression(self):
-		codeExpressionNode=self.getCodeElement()
-		return codeExpressionNode!=None
 
