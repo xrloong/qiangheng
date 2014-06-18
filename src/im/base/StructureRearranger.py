157e9f009223a51341dbea42fe62717018565987
xrloong <xrloong@gmail.com>
2014-06-19 01:31:14 +0800

    重構，移除因使用 TreeRegExp 而不使用的程式碼。

--- src/im/base/StructureRearranger.py
+++ src/im/base/StructureRearranger.py
@@ -44,7 +44,6 @@ class StructureRearranger:
 
 	def rearrangeDesc(self, structDesc):
 		self.rearrangeByTreeRegExp(structDesc)
-		self.rearrangeSpecial(structDesc)
 
 		operator=structDesc.getOperator()
 		while not operator.isBuiltin():
@@ -56,10 +55,6 @@ class StructureRearranger:
 				operator=structDesc.getOperator()
 			else:
 				break
-
-	def rearrangeSpecial(self, structDesc):
-		pass
-
 
 
 	def generateStructureDescription(self, structInfo=['龜', []]):
@@ -73,133 +68,6 @@ class StructureRearranger:
 		structDesc=self.generateStructureDescription()
 		structDesc.setReferenceExpression(name)
 		return structDesc
-
-
-	def getRearrangeListSilkworm(self):
-		# 蚕
-		rearrangeList=[
-			# 範例
-#			['一', '口', ['[一口]']],
-			]
-		return rearrangeList
-
-	def getRearrangeListZai(self):
-		# 載
-		rearrangeList=[
-			# 範例
-#			['戈', '口', ['[一口]', '[特戈]']],
-			]
-		return rearrangeList
-
-	def getRearrangeListFanFu(self):
-		# 範焤
-		rearrangeList=[
-			'厭', '辰', '麻'
-			]
-		return rearrangeList
-
-	def getRearrangeListFanYan(self):
-		# 範衍
-		rearrangeList=[
-			# 範例
-#			['儿.0', '儿.1', ['丨', '乚']],
-#			['儿.0', '儿.1', '儿'],
-			]
-		return rearrangeList
-
-
-	def rearrangeForSilkworm(self, structDesc):
-		compList=structDesc.getCompList()
-
-		rearrangeList=self.getRearrangeListSilkworm()
-
-		newCompList=compList
-		for [firstName, secondName, resultList] in rearrangeList:
-			compList=newCompList
-			newCompList=[]
-
-			firstChildStructDesc=None
-			secondChildStructDesc=compList[0]
-			for idx in range(1, len(compList)):
-				firstChildStructDesc=secondChildStructDesc
-				secondChildStructDesc=compList[idx]
-
-				if firstChildStructDesc==None or secondChildStructDesc==None:
-					continue
-
-				isMatch= firstChildStructDesc.getReferenceExpression()==firstName \
-					and secondChildStructDesc.getReferenceExpression()==secondName
-				if isMatch:
-					for resultName in resultList:
-						newStructDesc=self.generateStructureDescriptionWithName(resultName)
-						newCompList.append(newStructDesc)
-					firstChildStructDesc=None
-					secondChildStructDesc=None
-				else:
-					newCompList.append(firstChildStructDesc)
-			if secondChildStructDesc!=None:
-				newCompList.append(secondChildStructDesc)
-
-				structDescList=newCompList
-				structDesc.setCompList(structDescList)
-				structDesc.setOperator(Operator.OperatorSilkworm)
-
-	def rearrangeForZai(self, structDesc):
-		compList=structDesc.getCompList()
-
-		rearrangeList=self.getRearrangeListZai()
-
-		newCompList=compList
-		for [firstName, secondName, resultList] in rearrangeList:
-			firstChildStructDesc=compList[0]
-			secondChildStructDesc=compList[1]
-			isMatch= firstChildStructDesc.getReferenceExpression()==firstName \
-				and secondChildStructDesc.getReferenceExpression()==secondName
-			if isMatch:
-				rearName=resultList[-1]
-				newCompList=[]
-				for frontName in resultList[:-1]:
-					newFrontStructDesc=self.generateStructureDescriptionWithName(frontName)
-					newCompList.append(newFrontStructDesc)
-				newRearStructDesc=self.generateStructureDescriptionWithName(rearName)
-				newCompList.extend(compList[1:-1])
-				newCompList.append(newRearStructDesc)
-
-				structDescList=newCompList
-				structDesc.setCompList(structDescList)
-				structDesc.setOperator(Operator.OperatorLoong)
-
-	def rearrangeForFanFu(self, structDesc):
-		compList=structDesc.getCompList()
-		childStructDesc=compList[0]
-
-		rearrangeList=self.getRearrangeListFanFu()
-		if childStructDesc.getReferenceExpression() in rearrangeList:
-			structDesc.setOperator(Operator.OperatorLiao)
-
-	def rearrangeForFanYan(self, structDesc):
-		compList=structDesc.getCompList()
-
-		firstChhildStructDesc=compList[0]
-		lastChildStructDesc=compList[-1]
-
-		rearrangeList=self.getRearrangeListFanYan()
-		for [leftName, rightName, result] in rearrangeList:
-			isMatch= firstChhildStructDesc.getReferenceExpression()==leftName \
-				and lastChildStructDesc.getReferenceExpression()==rightName
-			if isMatch:
-				if type(result) is type([]):
-					[newLeftName, newRightName]=result
-					newLeftStructDesc=self.generateStructureDescriptionWithName(newLeftName)
-					newRightStructDesc=self.generateStructureDescriptionWithName(newRightName)
-					structDescList=[newLeftStructDesc]+compList[1:-1]+[newRightStructDesc]
-				else:
-					resultName=result
-					newStructDesc=self.generateStructureDescriptionWithName(resultName)
-					structDescList=[newStructDesc]+compList[1:-1]
-
-				structDesc.setCompList(structDescList)
-				structDesc.setOperator(Operator.OperatorGoose)
 
 	def rearrangeByTreeRegExp(self, structDesc):
 		compList=structDesc.getCompList()
@@ -258,25 +126,6 @@ class StructureRearranger:
 		operator=self.operatorGenerator(operatorName)
 		structDesc=StructureDescription.generate(operator, compList)
 		return (rest, structDesc)
-
-	def genStructDesc(self, tre, expression):
-		expression=expression[1:-1]
-		expressionList=expression.split()
-		operatorName=expressionList[0]
-
-		compList=[self.generateStructureDescriptionWithName(expression) for expression in expressionList[1:]]
-
-		compList=[]
-		for expression in expressionList[1:]:
-			if expression[:1]=='\\':
-				index=int(expression[1:])
-				compList.extend(tre.getComp(index).getMatched())
-			else:
-				compList.append(self.generateStructureDescriptionWithName(expression))
-
-		operator=self.operatorGenerator(operatorName)
-		structureDescription=StructureDescription.generate(operator, compList)
-		return structureDescription
 
 	def genStructDesc(self, tre, expression):
 		return self.genStructDescRecursive(tre, self.generateTokens(expression))[1]
