d9a7e169989bb1abc30bbae380ae89f6bc2a1cc2
Xanadu Wang <xrloong@gmail.com>
2019-03-29 12:21:58 +0800

    重構，將 model/im/FourCorner/* 放進 model/im/FourCorner.py 。

--- src/model/im/FourCorner.py
+++ src/model/im/FourCorner.py
@@ -1,7 +1,96 @@
-from .FCCodeInfo import FCCodeInfo
+from model.base.IMInfo import IMInfo
+from model.base.CodeInfo import CodeInfo
 from model.base.CodeInfoEncoder import CodeInfoEncoder
 
-import sys
+class FourCornerInfo(IMInfo):
+	"四角號碼"
+
+	IMName="四角"
+	def __init__(self):
+		self.keyMaps=[
+			['0', '0',],
+			['1', '1',],
+			['2', '2',],
+			['3', '3',],
+			['4', '4',],
+			['5', '5',],
+			['6', '6',],
+			['7', '7',],
+			['8', '8',],
+			['9', '9',],
+			]
+		self.nameDict={
+				'cn':'四角',
+				'tw':'四角',
+				'hk':'四角',
+				'en':'FourCourner',
+				}
+		self.iconfile="qhfc.svg"
+		self.maxkeylength=4
+
+class FCCodeInfo(CodeInfo):
+	STROKE_NONE=None
+	STROKE_0='0'
+	STROKE_1='1'
+	STROKE_2='2'
+	STROKE_3='3'
+	STROKE_4='4'
+	STROKE_5='5'
+	STROKE_6='6'
+	STROKE_7='7'
+	STROKE_8='8'
+	STROKE_9='9'
+
+	STROKES=[
+		STROKE_0,
+		STROKE_1,
+		STROKE_2,
+		STROKE_3,
+		STROKE_4,
+		STROKE_5,
+		STROKE_6,
+		STROKE_7,
+		STROKE_8,
+		STROKE_9,
+	]
+
+	CORNER_NONE=None
+	CORNER_TOP_LEFT='a'
+	CORNER_TOP_RIGHT='b'
+	CORNER_BOTTOM_LEFT='c'
+	CORNER_BOTTOM_RIGHT='d'
+
+	CORNERS=[
+		CORNER_TOP_LEFT,
+		CORNER_TOP_RIGHT,
+		CORNER_BOTTOM_LEFT,
+		CORNER_BOTTOM_RIGHT,
+	]
+
+	def __init__(self, corners):
+		super().__init__()
+		[self._top_left, self._top_right, self._bottom_left, self._bottom_right]=corners
+
+	@staticmethod
+	def generateDefaultCodeInfo(corners):
+		codeInfo=FCCodeInfo(corners)
+		return codeInfo
+
+	def toCode(self):
+		return "%s%s%s%s"%(self.getTopLeft(), self.getTopRight(), self.getBottomLeft(), self.getBottomRight())
+
+	def getTopLeft(self):
+		return self._top_left if self._top_left not in ['a', 'b', 'c'] else '0'
+
+	def getTopRight(self):
+		return self._top_right if self._top_right not in ['a', 'b', 'c'] else '0'
+
+	def getBottomLeft(self):
+		return self._bottom_left if self._bottom_left not in ['a', 'b', 'c'] else '0'
+
+	def getBottomRight(self):
+		return self._bottom_right if self._bottom_right not in ['a', 'b', 'c'] else '0'
+
 
 class FCCodeInfoEncoder(CodeInfoEncoder):
 	@classmethod
@@ -534,4 +623,37 @@ class FCGrid:
 
 	def getFourCorner(self):
 		return self.getStrokeCode()
+
+class FCRadixParser():
+	ATTRIB_CODE_EXPRESSION='資訊表示式'
+
+	# 多型
+	def convertRadixDescToCodeInfo(self, radixDesc):
+		codeInfo=self.convertRadixDescToCodeInfoByExpression(radixDesc)
+		return codeInfo
+
+	def convertRadixDescToCodeInfoByExpression(self, radixInfo):
+		elementCodeInfo=radixInfo.getCodeElement()
+
+		infoDict=elementCodeInfo
+
+		top_left=''
+		top_right=''
+		bottom_left=''
+		bottom_right=''
+
+		characterCode=infoDict.get(FCRadixParser.ATTRIB_CODE_EXPRESSION)
+		if len(characterCode)==4:
+			top_left=characterCode[0]
+			top_right=characterCode[1]
+			bottom_left=characterCode[2]
+			bottom_right=characterCode[3]
+
+		corners=[top_left, top_right, bottom_left, bottom_right]
+		codeInfo=FCCodeInfo(corners)
+		return codeInfo
+
+IMInfo = FourCornerInfo
+CodeInfoEncoder = FCCodeInfoEncoder
+RadixParser = FCRadixParser
 
--- src/model/im/FourCorner/__init__.py
+++
@@ -1,7 +0,0 @@
-from .FourCornerInfo import FourCornerInfo as IMInfo
-from .FCCodeInfoEncoder import FCCodeInfoEncoder as CodeInfoEncoder
-from .FCRadixManager import FCRadixParser as RadixParser
-
-if __name__=='__main__':
-	pass
-
