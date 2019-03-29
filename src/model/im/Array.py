e24a23f6a10db25efa46591ffa935b9da411c60f
Xanadu Wang <xrloong@gmail.com>
2019-03-29 12:04:10 +0800

    重構，將 model/im/Array/* 放進 model/im/Array.py 。

--- src/model/im/Array.py
+++ src/model/im/Array.py
@@ -1,7 +1,149 @@
-from .ARCodeInfo import ARCodeInfo
+from model.base.IMInfo import IMInfo
+from model.base.CodeInfo import CodeInfo
 from model.base.CodeInfoEncoder import CodeInfoEncoder
 
-import sys
+class ArrayInfo(IMInfo):
+	"行列輸入法"
+
+	IMName="行列"
+	def __init__(self):
+		self.keyMaps=[
+			['a', '1-',],
+			['b', '5v',],
+			['c', '3v',],
+			['d', '3-',],
+			['e', '3^',],
+			['f', '4-',],
+			['g', '5-',],
+			['h', '6-',],
+			['i', '8^',],
+			['j', '7-',],
+			['k', '8-',],
+			['l', '9-',],
+			['m', '7v',],
+			['n', '6v',],
+			['o', '9^',],
+			['p', '0^',],
+			['q', '1^',],
+			['r', '4^',],
+			['s', '2-',],
+			['t', '5^',],
+			['u', '7^',],
+			['v', '4v',],
+			['w', '2^',],
+			['x', '2v',],
+			['y', '6^',],
+			['z', '1v',],
+			['.', '9v',],
+			['/', '0v',],
+			[';', '0-',],
+			[',', '8v',],
+			]
+		self.nameDict={
+				'cn':'行列',
+				'tw':'行列',
+				'hk':'行列',
+				'en':'Array',
+				}
+		self.iconfile="qhar.svg"
+		self.maxkeylength=4
+
+class ARCodeInfo(CodeInfo):
+	RADIX_1_UP='1^'
+	RADIX_2_UP='2^'
+	RADIX_3_UP='3^'
+	RADIX_4_UP='4^'
+	RADIX_5_UP='5^'
+	RADIX_6_UP='6^'
+	RADIX_7_UP='7^'
+	RADIX_8_UP='8^'
+	RADIX_9_UP='9^'
+	RADIX_0_UP='0^'
+
+	RADIX_1_CENTER='1-'
+	RADIX_2_CENTER='2-'
+	RADIX_3_CENTER='3-'
+	RADIX_4_CENTER='4-'
+	RADIX_5_CENTER='5-'
+	RADIX_6_CENTER='6-'
+	RADIX_7_CENTER='7-'
+	RADIX_8_CENTER='8-'
+	RADIX_9_CENTER='9-'
+	RADIX_0_CENTER='0-'
+
+	RADIX_1_BOTTOM='1v'
+	RADIX_2_BOTTOM='2v'
+	RADIX_3_BOTTOM='3v'
+	RADIX_4_BOTTOM='4v'
+	RADIX_5_BOTTOM='5v'
+	RADIX_6_BOTTOM='6v'
+	RADIX_7_BOTTOM='7v'
+	RADIX_8_BOTTOM='8v'
+	RADIX_9_BOTTOM='9v'
+	RADIX_0_BOTTOM='0v'
+
+	radixToCodeDict={
+		RADIX_1_UP:'q',
+		RADIX_2_UP:'w',
+		RADIX_3_UP:'e',
+		RADIX_4_UP:'r',
+		RADIX_5_UP:'t',
+		RADIX_6_UP:'y',
+		RADIX_7_UP:'u',
+		RADIX_8_UP:'i',
+		RADIX_9_UP:'o',
+		RADIX_0_UP:'p',
+
+		RADIX_1_CENTER:'a',
+		RADIX_2_CENTER:'s',
+		RADIX_3_CENTER:'d',
+		RADIX_4_CENTER:'f',
+		RADIX_5_CENTER:'g',
+		RADIX_6_CENTER:'h',
+		RADIX_7_CENTER:'j',
+		RADIX_8_CENTER:'k',
+		RADIX_9_CENTER:'l',
+		RADIX_0_CENTER:';',
+
+		RADIX_1_BOTTOM:'z',
+		RADIX_2_BOTTOM:'x',
+		RADIX_3_BOTTOM:'c',
+		RADIX_4_BOTTOM:'v',
+		RADIX_5_BOTTOM:'b',
+		RADIX_6_BOTTOM:'n',
+		RADIX_7_BOTTOM:'m',
+		RADIX_8_BOTTOM:',',
+		RADIX_9_BOTTOM:'.',
+		RADIX_0_BOTTOM:'/',
+	}
+
+	def __init__(self, codeList=[]):
+		super().__init__()
+
+		self._codeList=codeList
+
+	@staticmethod
+	def generateDefaultCodeInfo(codeList):
+		codeInfo=ARCodeInfo(codeList)
+		return codeInfo
+
+	def toCode(self):
+		mainRadixList=self.getMainCodeList()
+		mainCodeList=list(map(lambda x: ARCodeInfo.radixToCodeDict[x], mainRadixList))
+		code="".join(mainCodeList)
+		return (code[:3]+code[-1] if len(code)>4 else code)
+
+
+	def isInstallmentEncoded(self):
+		return len(self._codeList)>1
+
+	def getMainCodeList(self):
+		if self._codeList != None:
+			return sum(self._codeList, [])
+		return None
+
+	def getInstallmentCode(self, index):
+		return self._codeList[index]
 
 class ARCodeInfoEncoder(CodeInfoEncoder):
 	@classmethod
@@ -189,4 +331,34 @@ class ARCodeInfoEncoder(CodeInfoEncoder):
 		cat=sum(arCodeList, [])
 		arCode=cat[:3]+cat[-1:] if len(cat)>4 else cat
 		return arCode
+
+class ARRadixParser():
+	INSTALLMENT_SEPERATOR='|'
+	RADIX_SEPERATOR=','
+
+	ATTRIB_CODE_EXPRESSION='資訊表示式'
+
+	# 多型
+	def convertRadixDescToCodeInfo(self, radixDesc):
+		codeInfo=self.convertRadixDescToCodeInfoByExpression(radixDesc)
+		return codeInfo
+
+	def convertRadixDescToCodeInfoByExpression(self, radixInfo):
+		elementCodeInfo=radixInfo.getCodeElement()
+
+		infoDict=elementCodeInfo
+
+		codeList=None
+
+		str_rtlist=infoDict.get(ARRadixParser.ATTRIB_CODE_EXPRESSION)
+		if str_rtlist!=None:
+			codeList=str_rtlist.split(ARRadixParser.INSTALLMENT_SEPERATOR)
+			codeList=list(map(lambda x: x.split(ARRadixParser.RADIX_SEPERATOR), codeList))
+
+		codeInfo=ARCodeInfo(codeList)
+		return codeInfo
+
+IMInfo = ArrayInfo
+CodeInfoEncoder = ARCodeInfoEncoder
+RadixParser = ARRadixParser
 
--- src/model/im/Array/__init__.py
+++
@@ -1,7 +0,0 @@
-from .ArrayInfo import ArrayInfo as IMInfo
-from .ARCodeInfoEncoder import ARCodeInfoEncoder as CodeInfoEncoder
-from .ARRadixManager import ARRadixParser as RadixParser
-
-if __name__=='__main__':
-	pass
-
