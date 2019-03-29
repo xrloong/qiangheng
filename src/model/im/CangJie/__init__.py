f0770df52493c1221e32a087f4b4720325d95941
Xanadu Wang <xrloong@gmail.com>
2019-03-29 12:14:35 +0800

    重構，將 model/im/CangJie/* 放進 model/im/CangJie.py 。

--- src/model/im/CangJie/__init__.py
+++
@@ -1,7 +0,0 @@
-from .CangJieInfo import CangJieInfo as IMInfo
-from .CJCodeInfoEncoder import CJCodeInfoEncoder as CodeInfoEncoder
-from .CJRadixManager import CJRadixParser as RadixParser
-
-if __name__=='__main__':
-	pass
-
