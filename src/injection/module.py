84af35270d4789cc8d2f8fe0b4471de7d7cb3687
Xanadu Wang <xrloong@gmail.com>
2019-03-29 19:31:52 +0800

    重構，將決定 Package 及 Writer 的相關移到更外層，即 qiangheng.py 。

--- src/injection/module.py
+++ src/injection/module.py
@@ -8,18 +8,10 @@ from model.CharacterDescriptionManager import CharacterDescriptionManager
 from model.hanzi import HanZiNetwork
 import model.base
 
-from Constant import MethodName, Package, IMName
-from Constant import IsForIm, Quiet, OutputFormat, Writer
+from Constant import Package, IMName
 from Constant import MainCharDescManager, ImCharDescManager
 
 class PackageModule(Module):
-	@provider
-	def providePackage(self, isForIm: IsForIm, methodName: MethodName) -> Package:
-		if isForIm:
-			return getImPackage(methodName)
-		else:
-			return getDmPackage(methodName)
-
 	@provider
 	def provideIMInfo(self, package: Package) -> model.base.IMInfo:
 		return package.IMInfo()
@@ -51,132 +43,4 @@ class PackageModule(Module):
 	@provider
 	def provideHanZiNetwork(self) -> HanZiNetwork:
 		return HanZiNetwork()
-
-	@provider
-	def provideWriter(self, isForIm: IsForIm, quiet: Quiet, outputFormat: OutputFormat) -> Writer:
-		self.isForIm = isForIm
-		return self.getWriter(quiet, outputFormat)
-
-	def getWriter(self, quiet=False, output_format="text"):
-		isFormatXML=(output_format=='xml')
-		isFormatYAML=(output_format=='yaml')
-		isFormatTXT=(output_format=='text')
-		isFormatQuiet=(output_format=='quiet')
-
-		isToOutput=not quiet and not isFormatQuiet
-		if isToOutput:
-			if isFormatXML:
-				writer = self.getXmlWriter()
-			elif isFormatYAML:
-				writer = self.getYamlWriter()
-			elif isFormatTXT:
-				writer = self.getTextWriter()
-			else:
-				writer = self.getTextWriter()
-		else:
-			# 不輸出結果
-			writer = self.getQuietWriter()
-		return writer
-
-	def getTextWriter(self):
-		if self.isForIm:
-			from writer.im import TxtWriter
-		else:
-			from writer.dm import TxtWriter
-		writer = TxtWriter()
-		return writer
-
-	def getXmlWriter(self):
-		if self.isForIm:
-			from writer.im import XmlWriter
-		else:
-			from writer.dm import XmlWriter
-		writer = XmlWriter()
-		return writer
-
-	def getYamlWriter(self):
-		if self.isForIm:
-			from writer.im import YamlWriter
-		else:
-			from writer.dm import YamlWriter
-		writer = YamlWriter()
-		return writer
-
-	def getQuietWriter(self):
-		if self.isForIm:
-			from writer.im import QuietWriter
-		else:
-			from writer.dm import QuietWriter
-		writer = QuietWriter()
-		return writer
-
-def getImPackage(imName):
-	if imName in ['倉', '倉頡', '倉頡輸入法', 'cangjie', 'cj',]:
-		imName='倉頡'
-	elif imName in ['行', '行列', '行列輸入法', 'array', 'ar',]:
-		imName='行列'
-	elif imName in ['易', '大易', '大易輸入法', 'dayi', 'dy',]:
-		imName='大易'
-	elif imName in ['嘸', '嘸蝦米', '嘸蝦米輸入法', 'boshiamy', 'bs',]:
-		imName='嘸蝦米'
-	elif imName in ['鄭', '鄭碼', '鄭碼輸入法', 'zhengma', 'zm',]:
-		imName='鄭碼'
-	elif imName in ['四', '四角', '四角號碼', 'fourcorner', 'fc',]:
-		imName='四角'
-	elif imName in ['庋', '庋㩪', '中國字庋㩪', 'guixie', 'gx',]:
-		imName='庋㩪'
-	elif imName in ['例', '範例', '範例輸入法', 'sample', 'sample',]:
-		imName='範例'
-	else:
-		assert False, "不知道的輸入法: %s"%imName
-		imName='不知道'
-
-	if imName == '倉頡':
-		from model.im import CangJie
-		imPackage=CangJie
-	elif imName == '行列':
-		from model.im import Array
-		imPackage=Array
-	elif imName == '大易':
-		from model.im import DaYi
-		imPackage=DaYi
-	elif imName == '嘸蝦米':
-		from model.im import Boshiamy
-		imPackage=Boshiamy
-	elif imName == '鄭碼':
-		from model.im import ZhengMa
-		imPackage=ZhengMa
-	elif imName == '四角':
-		from model.im import FourCorner
-		imPackage=FourCorner
-	elif imName == '庋㩪':
-		from model.im import GuiXie
-		imPackage=GuiXie
-	elif imName == '範例':
-		from model.im import Sample
-		imPackage=Sample
-	else:
-		assert False, "不知道的輸入法: %s"%imName
-
-	return imPackage
-
-def getDmPackage(dmName):
-	if dmName in ['動', '動組', '動態組字', 'dynamiccomposition', 'dc',]:
-		dmName='動組'
-	elif dmName in ['筆順', 'strokeorder', 'so',]:
-		dmName='筆順'
-	else:
-		assert False, "不知道的繪字法: %s"%dmName
-		dmName='不知道'
-
-	if dmName == '動組':
-		from model.dm import DynamicComposition
-		dmPackage=DynamicComposition
-	elif dmName == '筆順':
-		from model.dm import StrokeOrder
-		dmPackage=StrokeOrder
-	else:
-		assert False, "不知道的繪字法: %s"%dmName
-
-	return dmPackage
 
--- src/qiangheng.py
+++ src/qiangheng.py
@@ -7,7 +7,8 @@ from injector import inject
 from optparse import OptionParser
 
 from injection.module import PackageModule
-from Constant import MethodName, IsForIm, Quiet, OutputFormat
+from Constant import Package, MethodName, IsForIm
+from Constant import Quiet, OutputFormat
 from Constant import Writer
 
 from model.base.IMInfo import IMInfo
@@ -31,17 +32,152 @@ class QiangHeng:
 			methodName = drawMethod
 			isForIm = False
 
+		package = self.computePackage(isForIm, methodName)
+		writer = self.computeWriter(isForIm, quiet, output_format)
+
 		def configure(binder):
 			binder.bind(MethodName, to=methodName)
-			binder.bind(IsForIm, to=isForIm)
-			binder.bind(Quiet, to=quiet)
-			binder.bind(OutputFormat, to=output_format)
+			binder.bind(Package, to=package)
+			binder.bind(Writer, to=writer)
 
 		injector = Injector([configure, PackageModule()])
 
 		mainManager=injector.get(MainManager)
 		mainManager.compute()
 		mainManager.write()
+
+	def computePackage(self, isForIm, methodName):
+		if isForIm:
+			return self.getImPackage(methodName)
+		else:
+			return self.getDmPackage(methodName)
+
+	def getImPackage(self, imName):
+		if imName in ['倉', '倉頡', '倉頡輸入法', 'cangjie', 'cj',]:
+			imName='倉頡'
+		elif imName in ['行', '行列', '行列輸入法', 'array', 'ar',]:
+			imName='行列'
+		elif imName in ['易', '大易', '大易輸入法', 'dayi', 'dy',]:
+			imName='大易'
+		elif imName in ['嘸', '嘸蝦米', '嘸蝦米輸入法', 'boshiamy', 'bs',]:
+			imName='嘸蝦米'
+		elif imName in ['鄭', '鄭碼', '鄭碼輸入法', 'zhengma', 'zm',]:
+			imName='鄭碼'
+		elif imName in ['四', '四角', '四角號碼', 'fourcorner', 'fc',]:
+			imName='四角'
+		elif imName in ['庋', '庋㩪', '中國字庋㩪', 'guixie', 'gx',]:
+			imName='庋㩪'
+		elif imName in ['例', '範例', '範例輸入法', 'sample', 'sample',]:
+			imName='範例'
+		else:
+			assert False, "不知道的輸入法: %s"%imName
+			imName='不知道'
+
+		if imName == '倉頡':
+			from model.im import CangJie
+			imPackage=CangJie
+		elif imName == '行列':
+			from model.im import Array
+			imPackage=Array
+		elif imName == '大易':
+			from model.im import DaYi
+			imPackage=DaYi
+		elif imName == '嘸蝦米':
+			from model.im import Boshiamy
+			imPackage=Boshiamy
+		elif imName == '鄭碼':
+			from model.im import ZhengMa
+			imPackage=ZhengMa
+		elif imName == '四角':
+			from model.im import FourCorner
+			imPackage=FourCorner
+		elif imName == '庋㩪':
+			from model.im import GuiXie
+			imPackage=GuiXie
+		elif imName == '範例':
+			from model.im import Sample
+			imPackage=Sample
+		else:
+			assert False, "不知道的輸入法: %s"%imName
+
+		return imPackage
+
+	def getDmPackage(self, dmName):
+		if dmName in ['動', '動組', '動態組字', 'dynamiccomposition', 'dc',]:
+			dmName='動組'
+		elif dmName in ['筆順', 'strokeorder', 'so',]:
+			dmName='筆順'
+		else:
+			assert False, "不知道的繪字法: %s"%dmName
+			dmName='不知道'
+
+		if dmName == '動組':
+			from model.dm import DynamicComposition
+			dmPackage=DynamicComposition
+		elif dmName == '筆順':
+			from model.dm import StrokeOrder
+			dmPackage=StrokeOrder
+		else:
+			assert False, "不知道的繪字法: %s"%dmName
+
+		return dmPackage
+
+	def computeWriter(self, isForIm: IsForIm, quiet: Quiet, outputFormat: OutputFormat) -> Writer:
+		self.isForIm = isForIm
+		return self.getWriter(quiet, outputFormat)
+
+	def getWriter(self, quiet=False, output_format="text"):
+		isFormatXML=(output_format=='xml')
+		isFormatYAML=(output_format=='yaml')
+		isFormatTXT=(output_format=='text')
+		isFormatQuiet=(output_format=='quiet')
+
+		isToOutput=not quiet and not isFormatQuiet
+		if isToOutput:
+			if isFormatXML:
+				writer = self.getXmlWriter()
+			elif isFormatYAML:
+				writer = self.getYamlWriter()
+			elif isFormatTXT:
+				writer = self.getTextWriter()
+			else:
+				writer = self.getTextWriter()
+		else:
+			# 不輸出結果
+			writer = self.getQuietWriter()
+		return writer
+
+	def getTextWriter(self):
+		if self.isForIm:
+			from writer.im import TxtWriter
+		else:
+			from writer.dm import TxtWriter
+		writer = TxtWriter()
+		return writer
+
+	def getXmlWriter(self):
+		if self.isForIm:
+			from writer.im import XmlWriter
+		else:
+			from writer.dm import XmlWriter
+		writer = XmlWriter()
+		return writer
+
+	def getYamlWriter(self):
+		if self.isForIm:
+			from writer.im import YamlWriter
+		else:
+			from writer.dm import YamlWriter
+		writer = YamlWriter()
+		return writer
+
+	def getQuietWriter(self):
+		if self.isForIm:
+			from writer.im import QuietWriter
+		else:
+			from writer.dm import QuietWriter
+		writer = QuietWriter()
+		return writer
 
 class MainManager:
 	@inject
