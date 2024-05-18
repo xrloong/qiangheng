d053bd5fa92cae9c4ae38c6713dc66ce852d0702
Xanadu Wang <xrloong@gmail.com>
2024-05-18 15:16:14 +0800

    風格，整理 CharacterInfo.py 的風格

--- src/model/element/CharacterInfo.py
+++ src/model/element/CharacterInfo.py
@@ -2,22 +2,22 @@ from . import CodeMappingInfo
 
 class CharacterInfo:
 	def __init__(self, charName):
-		self.name=charName
+		self.name = charName
 
 	@property
 	def character(self):
 		return self.name
 
 	def setCodeList(self, codeList):
-		self.codePropList=codeList
+		self.codePropList = codeList
 
 	def getCodeMappingInfoList(self):
-		characterInfoList=[]
+		characterInfoList = []
 		for codeAndType in self.codePropList:
-			code, variance=codeAndType
-			characterInfo=CodeMappingInfo.CodeMappingInfo(self.name, code, variance)
+			code, variance = codeAndType
+			characterInfo = CodeMappingInfo.CodeMappingInfo(self.name, code, variance)
 			characterInfoList.append(characterInfo)
-		self.characterInfoList=characterInfoList
+		self.characterInfoList = characterInfoList
 
 		return self.characterInfoList
 
