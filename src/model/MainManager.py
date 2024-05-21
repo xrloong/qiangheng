225798a0e9612efb43fd8fc27d6411aaa59a6094
Xanadu Wang <xrloong@gmail.com>
2024-05-21 12:11:51 +0800

    重構，整理 MainManager

--- src/model/MainManager.py
+++ src/model/MainManager.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from injector import inject
+from injector import Injector
 
 from injection.key import Writer
 from injection.key import Characters
@@ -13,31 +14,31 @@ from hanzi.converter import ComputeCharacter
 
 class MainManager:
 	@inject
-	def __init__(self,
-			characters: Characters,
-			constructCharacter: ConstructCharacter,
-			computeCharacter: ComputeCharacter,
-			codeMappingInfoInterpreter: CodeMappingInfoInterpreter,
-			writer: Writer,
-			):
-		self.__characters = characters
-
-		self.__constructCharacter = constructCharacter
-		self.__computeCharacter = computeCharacter
-		self.__codeMappingInfoInterpreter = codeMappingInfoInterpreter
-		self.__writer = writer
+	def __init__(self, injector: Injector):
+		self.__injector = injector
 
 	def work(self):
 		self.__compute()
 		self.__write()
 
 	def __compute(self):
-		self.__constructCharacter.compute(self.__characters)
-		self.__constructCharacter.appendFastCodes()
+		injector = self.__injector
+
+		characters = injector.get(Characters)
+		constructCharacter = injector.get(ConstructCharacter)
+		constructCharacter.compute(characters)
+		constructCharacter.appendFastCodes()
 
 	def __write(self):
-		characterInfos = self.__computeCharacter.compute(self.__characters)
+		injector = self.__injector
+
+		characters = injector.get(Characters)
+		computeCharacter = injector.get(ComputeCharacter)
+
+		characterInfos = computeCharacter.compute(characters)
 		characterInfos = tuple(sorted(characterInfos, key=lambda c: c.character))
 
-		self.__writer.write(characterInfos, self.__codeMappingInfoInterpreter)
+		codeMappingInfoInterpreter = injector.get(CodeMappingInfoInterpreter)
+		writer = injector.get(Writer)
+		writer.write(characterInfos, codeMappingInfoInterpreter)
 
