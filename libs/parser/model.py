75e003bb64c97a61404861ed2f6d1bd975a282c0
Xanadu Wang <xrloong@gmail.com>
2024-05-25 08:40:39 +0800

    對 pydantic 相關 model ，使用 model_config 機制

--- libs/parser/model.py
+++ libs/parser/model.py
@@ -2,10 +2,13 @@ from typing import Optional
 
 from pydantic import BaseModel
 from pydantic import Field
+from pydantic import ConfigDict
 
 from model.element.CodeVariance import CodeVariance
 
 class RadixCodeInfoModel(BaseModel):
+	model_config = ConfigDict(frozen = True)
+
 	varianceString: Optional[str] = Field(alias = '類型', default = None)
 	supportCharacterCode: Optional[str] = Field(
             alias = '字符碼', pattern = "是",
@@ -26,9 +29,13 @@ class RadixCodeInfoModel(BaseModel):
 		return not self.__hasSupportCharacterCode
 
 class SubstituteRuleModel(BaseModel):
+	model_config = ConfigDict(frozen = True)
+
 	pattern: str = Field(alias = '比對')
 	replacement: str = Field(alias = '替換')
 
 class SubstituteRuleSetModel(BaseModel):
+	model_config = ConfigDict(frozen = True)
+
 	rules: list[SubstituteRuleModel] = Field(alias = '規則集')
 
