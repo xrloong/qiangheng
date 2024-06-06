f6584f36c26782c2c76d5d8adca271cb2819e9c5
Xanadu Wang <xrloong@gmail.com>
2024-06-06 12:06:51 +0800

    風格，更新 model/element/StructureDescription.py 風格

--- src/model/element/StructureDescription.py
+++ src/model/element/StructureDescription.py
@@ -9,15 +9,15 @@ class StructureDescription:
 	def __init__(self, operator, compList):
 		self.fontVariance = FontVariance.All
 
-		self.referenceExpression=None
+		self.referenceExpression = None
 
-		self.flagIsRoot=False
+		self.flagIsRoot = False
 
-		self.operator=operator
-		self.compList=compList
+		self.operator = operator
+		self.compList = compList
 
 	def __str__(self):
-		return '<{0}={1}|({2})>'.format(self.getReferenceExpression(), self.getOperator().getName(), ",".join(map(str, self.getCompList())))
+		return '<{0} = {1}|({2})>'.format(self.getReferenceExpression(), self.getOperator().getName(), ",".join(map(str, self.getCompList())))
 
 	def __repr__(self):
 		return str(self)
@@ -33,20 +33,20 @@ class StructureDescription:
 
 	def generateName(self):
 		if self.isLeaf():
-			self.name=self.getReferenceExpression()
+			self.name = self.getReferenceExpression()
 		else:
 			strList = [self.getOperator().getName()]
 			strList.extend([comp.getUniqueName() for comp in self.compList])
-			self.name="({0})".format(" ".join(strList))
+			self.name = "({0})".format(" ".join(strList))
 
 	def setReferenceExpression(self, referenceExpression):
-		self.referenceExpression=referenceExpression
+		self.referenceExpression = referenceExpression
 
 	def getReferenceExpression(self):
 		return self.referenceExpression
 
 	def getReferenceName(self):
-		expression=self.referenceExpression
+		expression = self.referenceExpression
 		if expression:
 			return expression.split(".")[0]
 		else:
@@ -56,17 +56,17 @@ class StructureDescription:
 		return bool(self.getReferenceName())
 
 	def isEmpty(self):
-		return self.getOperator().getName()=='龜' or len(self.compList)==0
+		return self.getOperator().getName() == '龜' or len(self.compList) == 0
 
 	def setOperator(self, operator):
-		self.operator=operator
+		self.operator = operator
 		self.generateName()
 
 	def getOperator(self):
 		return self.operator
 
 	def setCompList(self, compList):
-		self.compList=compList
+		self.compList = compList
 		self.generateName()
 
 	def getCompList(self):
