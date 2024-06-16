dc4db44caba247b53b470a79023d06628b0df4e6
Xanadu Wang <xrloong@gmail.com>
2024-06-16 16:58:09 +0800

    重構，將 TreeRegExpInterpreter.replace() 移至 HanZiTreeRegExpInterpreter

--- libs/tree/regexp/interpreter.py
+++ libs/tree/regexp/interpreter.py
@@ -1,84 +1,11 @@
 from .item import TreeRegExp
 from .item import MatchResult
 from .item import BasicTreeProxy
-from .item import TreeNodeGenerator
 
 
 class TreeRegExpInterpreter:
-    def __init__(self, proxy: BasicTreeProxy, treeNodeGenerator: TreeNodeGenerator):
+    def __init__(self, proxy: BasicTreeProxy):
         self.proxy = proxy
-        self.treeNodeGenerator = treeNodeGenerator
-
-    def replace(self, tre: TreeRegExp, result: str):
-        def generateTokens(expression):
-            tokens = []
-            length = len(expression)
-            i = 0
-            while i < length:
-                if expression[i] in ["(", ")"]:
-                    tokens.append(expression[i])
-                    i += 1
-                elif expression[i] == "\\":
-                    j = i + 1
-                    while (
-                        j < length and expression[j].isdigit() or expression[j] == "."
-                    ):
-                        j += 1
-                    tokens.append(expression[i:j])
-                    i = j
-                elif expression[i] == " ":
-                    i += 1
-                else:
-                    j = i + 1
-                    while j < length and expression[j] not in ["(", ")", "\\", " "]:
-                        j += 1
-                    tokens.append(expression[i:j])
-                    i = j
-            return tokens
-
-        def genStructDesc(expression, allComps):
-            return genStructDescRecursive(generateTokens(expression), allComps)[1]
-
-        def genStructDescRecursive(tokens, allComps):
-            if not tokens[0] == "(":
-                return ([], None)
-
-            treeNodeGenerator = self.treeNodeGenerator
-            operatorName = tokens[1]
-            compList = []
-            rest = tokens[2:]
-            while len(rest) > 0:
-                if rest[0] == "(":
-                    rest, structDesc = genStructDescRecursive(rest, allComps)
-                    if structDesc is not None:
-                        compList.append(structDesc)
-                elif rest[0] == ")":
-                    rest = rest[1:]
-                    break
-                elif rest[0][:1] == "\\":
-                    refExp = rest[0][1:]
-                    refExpList = refExp.split(".")
-                    if len(refExpList) < 2:
-                        # \1
-                        index = int(refExpList[0])
-                        compList.extend(allComps[index].getMatched())
-                    else:
-                        # \1.1
-                        index = int(refExpList[0])
-                        subIndex = int(refExpList[1])
-                        referenceNode = allComps[index].getMatched()[0]
-                        node = treeNodeGenerator.generateLeafNodeByReference(
-                            referenceNode, subIndex
-                        )
-                        compList.append(node)
-                    rest = rest[1:]
-                else:
-                    compList.append(treeNodeGenerator.generateLeafNode(rest[0]))
-                    rest = rest[1:]
-            structDesc = treeNodeGenerator.generateNode(operatorName, compList)
-            return (rest, structDesc)
-
-        return genStructDesc(result, tre.getAll())
 
     def match(self, tre: TreeRegExp, node):
         return self.matchTree(tre, node)
